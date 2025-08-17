b48b25549f1fb0dda41b65cd829e8a57bf0375c3
JagannM <jthedarkknight@gmail.com>
2025-08-17 20:51:19 +0530

    Update function

--- update.js
+++ update.js
@@ -17,20 +17,31 @@ const courseSchema = new mongoose.Schema({
 
 //Model the schema
 const Course = mongoose.model("course", courseSchema);
-//update
-async function updateCourse(id) {
+//query first and then update
+/* async function updateCourse(id) {
   //findbyid
   const course = await Course.findById(id);
   if (!course) return;
   //modify
-  course.isPublished = true;
-  course.author = "another author";
-  /* course.set({
+  //course.isPublished = true;
+  //course.author = "another author";
+  course.set({
     isPublished: true,
     author: "another author",
-  }); */
+  });
   //save
   const result = await course.save();
+  console.log(result);
+} */
+//updateCourse("689d5b52787ad41ae02fd833");
+
+//update first approach
+async function updateCourse(id) {
+  const result = await Course.updateMany(
+    { _id: id },
+    { $set: { author: "Mosh", isPublished: false } }
+  );
+
   console.log(result);
 }
 updateCourse("689d5b52787ad41ae02fd833");
